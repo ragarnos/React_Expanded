c01261a4d364c20afc4774afb03862068bbafdb0
Eduard Vasyliev <ragarnos12@gmail.com>
2023-11-15 18:00:21 +0200

    Counter

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,17 @@
-import { cources } from "../assets/data/data";
-import CourseCard from "./CourseCard";
+// import { cources } from "../assets/data/data";
+// import CourseCard from "./CourseCard";
+
+import Counter from "./counter/Counter";
 
 
 function Content() {
   return (
     <>
       <div className="d-flex container">
-        {cources.map(course => (
+        <Counter />
+        {/* {cources.map(course => (
           <CourseCard key={course.id} course={course} />
-        ))}
+        ))} */}
       </div>
       
     </>
