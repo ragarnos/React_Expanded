bf6679cbb3cd3f4745e26d673a2b474338b191aa
Eduard Vasyliev <ragarnos12@gmail.com>
2023-12-16 13:48:12 +0200

    Redux Toolkit Counter

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./assets/styles/App.scss";
 import Home from "./components/pages/Home";
 import AllUsers from "./components/pages/Allusers";
 import User from "./components/pages/User";
+import Counter from "./components/Counter";
 function App() {
 
   return (
@@ -11,10 +12,12 @@ function App() {
       <ul className="horizontal">
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/users'>User</NavLink></li>
+        <li><NavLink to='/counter'>Counter</NavLink></li>
       </ul>
     </nav>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/counter' element={<Counter />} />
           <Route path="/users" element={<AllUsers />} />
           <Route path="/users/:userId" element={<User />} /> 
         </Routes>
---
+++ src/components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "./features/counterSlice";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
